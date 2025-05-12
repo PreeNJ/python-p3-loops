cd5bd7f921cd81938307fecb9d7f601d619c61cd
Priscillah Njai <priscillahnjaii@gmail.com>
2025-05-12 19:56:51 +0300

    Updated code using while anf for loop

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    for i in range(len(int_list)):
+        int_list[i] = int_list[i] ** 2
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
